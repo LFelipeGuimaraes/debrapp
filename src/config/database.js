800b5d1ecf1856719990f40b57ea8a3dfd28fbf4
Felipe Guimarães <luiz.guimaraes@dcomp.sor.ufscar.br>
2019-11-19 10:25:34 -0300

    teste

--- src/config/database.js
+++ src/config/database.js
@@ -4,7 +4,7 @@ const node_env = process.env.NODE_ENV
 module.exports = {
     dialect: 'postgres',
     host: process.env.DB_HOST || 'ec2-54-227-249-108.compute-1.amazonaws.com',
-    port: process.env.DB_PORT || '25432',
+    port: process.env.DB_PORT || '5432',
     username: process.env.POSTGRES_USER || 'jamwzcqfgaoagp',
     password: process.env.POSTGRES_PASSWORD || '23c65ac5c32e77280064ca03cb3f3a230208650a958b326de460f3078342516a',
     database: 'dcf2hmbgls3v7s',
