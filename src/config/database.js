c21ea7b4e8bcb8ac49e9006f3b0ef26cb0472e9f
Felipe Guimarães <luiz.guimaraes@dcomp.sor.ufscar.br>
2019-11-19 10:22:06 -0300

    teste

--- src/config/database.js
+++ src/config/database.js
@@ -3,13 +3,15 @@ const node_env = process.env.NODE_ENV
 
 module.exports = {
     dialect: 'postgres',
-    host: process.env.DB_HOST || 'localhost',
-    port: process.env.DB_PORT || 5432,
-    username: process.env.POSTGRES_USER || 'docker',
-    password: process.env.POSTGRES_PASSWORD || '123',
-    database: 'debra',
+    host: process.env.DB_HOST || 'ec2-54-227-249-108.compute-1.amazonaws.com',
+    port: process.env.DB_PORT || '25432',
+    username: process.env.POSTGRES_USER || 'jamwzcqfgaoagp',
+    password: process.env.POSTGRES_PASSWORD || '23c65ac5c32e77280064ca03cb3f3a230208650a958b326de460f3078342516a',
+    database: 'dcf2hmbgls3v7s',
     define: {
         timestamps: true,
         underscored: true,
     },
 };
+
+postgres://jamwzcqfgaoagp:23c65ac5c32e77280064ca03cb3f3a230208650a958b326de460f3078342516a@ec2-54-227-249-108.compute-1.amazonaws.com:5432/dcf2hmbgls3v7s
