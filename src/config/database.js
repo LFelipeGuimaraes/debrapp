03f39e483f8b23259274b3bdbceb18005b879869
Felipe Guimarães <luiz.guimaraes@dcomp.sor.ufscar.br>
2020-02-25 18:23:43 -0300

    changed db credentials

--- src/config/database.js
+++ src/config/database.js
@@ -3,11 +3,11 @@ require('custom-env').env(node_env);
 
 module.exports = {
     dialect: 'postgres',
-    host: process.env.DB_HOST || 'ec2-54-227-249-108.compute-1.amazonaws.com',
+    host: process.env.DB_HOST || 'ec2-184-72-235-159.compute-1.amazonaws.com',
     port: process.env.DB_PORT || 5432,
-    username: process.env.POSTGRES_USER || 'jamwzcqfgaoagp',
-    password: process.env.POSTGRES_PASSWORD || '23c65ac5c32e77280064ca03cb3f3a230208650a958b326de460f3078342516a',
-    database: process.env.DB_NAME || 'dcf2hmbgls3v7s',
+    username: process.env.POSTGRES_USER || 'xhrotatryfzirg',
+    password: process.env.POSTGRES_PASSWORD || 'a98dd73fae4b60ad527ed8f8a85a87ccb5d882349c64ea61920ecc6e0c5f6bb0',
+    database: process.env.DB_NAME || 'df89ln4n2j55it',
     define: {
         timestamps: true,
         underscored: true,
